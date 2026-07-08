1d9623f52c2fb6b1d26446a2c4fb120d535236dc
Wither__ <hhex16@gmail.com>
2026-07-08 11:53:02 +0200

    stdio.h: added input functions

--- include/stdio.h
+++ include/stdio.h
@@ -2,8 +2,15 @@
 
 #include <stdarg.h>
 
+/// We use const char * for streams since RadishOS does not have complex
+/// file structures yet, and no streams/file descriptors support.
+
 int fputc(int c, const char *stream);
 int putchar(int c);
+
+int fgetc(const char *stream);
+int getchar();
+char *fgets(char *restrict str, int count, const char *restrict stream);
 
 int vfprintf(const char *restrict stream, const char *restrict format, va_list arg);
 int vprintf(const char *restrict format, va_list arg);
