de2054627c34bd5630ba569d888636ae132a7e70
Wither__ <hhex16@gmail.com>
2026-07-08 11:53:55 +0200

    unistd: changed signatures

--- src/unistd.c
+++ src/unistd.c
@@ -1,6 +1,7 @@
 #include "unistd.h"
 #include <stddef.h>
 
+/* ------------------------------------------------------------------------------------------------- */
 int write(const char *file, const void *buf, size_t n)
 {
         __asm__ volatile (
@@ -13,7 +14,9 @@ int write(const char *file, const void *buf, size_t n)
         __asm__ volatile ("" : "=a"(ret));
         return ret;
 }
+/* ------------------------------------------------------------------------------------------------- */
 
+/* ------------------------------------------------------------------------------------------------- */
 int read(const char *file, void *buf, size_t n)
 {
         __asm__ volatile (
@@ -21,8 +24,9 @@ int read(const char *file, void *buf, size_t n)
                 ::
                 "a"(1), "b"(file), "c"(buf), "d"(n)
         );
-
+        
         int ret = 0;
         __asm__ volatile ("" : "=a"(ret));
         return ret;
 }
+/* ------------------------------------------------------------------------------------------------- */
