29e462ff93808e39a2e473de173167eaea58e0d9
Wither__ <hhex16@gmail.com>
2026-07-27 12:39:48 +0200

    crt0: open stdin and stdout

--- src/crt0.asm
+++ src/crt0.asm
@@ -1,10 +1,23 @@
 [bits 64]
 
 section .text
-global _start
-extern main
+global _start, stdin_fd, stdout_fd
+extern main, tomato_init
 
 _start:
+        ; open stdin
+        mov rax, 7
+        mov rbx, stdin_path
+        int 0x80
+        mov [stdin_fd], eax
+        ; open stdout
+        mov rax, 7
+        mov rbx, stdout_path
+        int 0x80
+        mov [stdout_fd], eax
+
+        call tomato_init
+
         call main
 
         mov rax, 4
@@ -13,3 +26,11 @@ _start:
 ; should never be reached
 hang:
         jmp hang
+
+section .rodata
+stdin_path:             db "D:/input"
+stdout_path:            db "D:/console"
+
+section .data
+stdin_fd:               dd 1
+stdout_fd:              dd 1
