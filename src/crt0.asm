daa8ef6583ff7745e766aa51c1af0b5896ea4242
Wither__ <hhex16@gmail.com>
2026-07-28 19:16:06 +0200

    crt0: only call C functions

--- src/crt0.asm
+++ src/crt0.asm
@@ -2,35 +2,13 @@
 
 section .text
 global _start, stdin_fd, stdout_fd
-extern main, tomato_init
+extern main, tomato_init, tomato_end
 
 _start:
-        ; open stdin
-        mov rax, 7
-        mov rbx, stdin_path
-        int 0x80
-        mov [stdin_fd], eax
-        ; open stdout
-        mov rax, 7
-        mov rbx, stdout_path
-        int 0x80
-        mov [stdout_fd], eax
-
         call tomato_init
-
         call main
-
-        mov rax, 4
-        int 0x80
+        call tomato_end
         
 ; should never be reached
 hang:
         jmp hang
-
-section .rodata
-stdin_path:             db "D:/input"
-stdout_path:            db "D:/console"
-
-section .data
-stdin_fd:               dd 1
-stdout_fd:              dd 1
