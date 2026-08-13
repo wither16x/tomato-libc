64037560a0557726b8a42458e08031b08c8b8f8d
Wither__ <hhex16@gmail.com>
2026-08-13 17:14:31 +0200

    string: organized sources

---
+++ src/string/strcmp.c
@@ -0,0 +1,36 @@
+#include "string.h"
+
+#ifdef __cplusplus
+extern "C"
+#endif
+
+/* ------------------------------------------------------------------------------------------------- */
+int strcmp(const char *lhs, const char *rhs)
+{
+        while (*lhs && (*lhs == *rhs)) {
+                lhs++;
+                rhs++;
+        }
+        return *(unsigned char *)lhs - *(unsigned char *)rhs;
+}
+/* ------------------------------------------------------------------------------------------------- */
+
+/* ------------------------------------------------------------------------------------------------- */
+int strncmp(const char *lhs, const char *rhs, size_t count)
+{
+        while (count &&  *lhs && (*lhs == *rhs)) {
+                ++lhs;
+                ++rhs;
+                --count;
+        }
+
+        if (count == 0)
+                return 0;
+
+        return *(unsigned char *)lhs - *(unsigned char *)rhs;
+}
+/* ------------------------------------------------------------------------------------------------- */
+
+#ifdef __cplusplus
+}
+#endif
---
+++ src/string/strcpy.c
@@ -0,0 +1,49 @@
+#include "string.h"
+
+#ifdef __cplusplus
+extern "C"
+#endif
+
+/* ------------------------------------------------------------------------------------------------- */
+char *strcpy(char *dest, const char *src)
+{
+        if (!dest)
+                return NULL;
+
+        char *p = dest;
+
+        while (*src != '\0') {
+                *dest = *src;
+                dest++;
+                src++;
+        }
+
+        *dest = '\0';
+
+        return p;
+}
+/* ------------------------------------------------------------------------------------------------- */
+
+/* ------------------------------------------------------------------------------------------------- */
+char *strncpy(char *restrict dest, const char *restrict src, size_t count)
+{
+        if(!dest)
+                return NULL;
+
+        char *p = dest;
+
+        while (*src && count--) {
+                *dest = *src;
+                dest++;
+                src++;
+        }
+
+        *dest = '\0';
+
+        return p;
+}
+/* ------------------------------------------------------------------------------------------------- */
+
+#ifdef __cplusplus
+}
+#endif
