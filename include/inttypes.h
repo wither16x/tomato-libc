aef6980a024d0a02b3ac74844cc8c8e5564422f1
Wither__ <hhex16@gmail.com>
2026-08-14 14:04:26 +0200

    inttypes: added printf format macros

--- include/inttypes.h
+++ include/inttypes.h
@@ -2,6 +2,28 @@
 
 #include <stdint.h>
 
+#define PRIdMAX "lld"
+#define PRIiMAX "lld"
+#define PRIoMAX "llo"
+#define PRIuMAX "llu"
+#define PRIxMAX "llx"
+#define PRIXMAX "llX"
+
+#define PRId8   "hhd"
+#define PRId16  "hd"
+#define PRId32  "d"
+#define PRId64  "lld"
+
+#define PRIu8   "hhu"
+#define PRIu16  "hu"
+#define PRIu32  "u"
+#define PRIu64  "llu"
+
+#define PRIx8   "hhx"
+#define PRIx16  "hx"
+#define PRIx32  "x"
+#define PRIx64  "llx"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
